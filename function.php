23030bda9ef6b96da954f50a0a95d3f9f567de18
kaua sousa <kaua.sousa@catskillet.com>
2022-08-22 16:59:21 -0300

    Primeiro save

---
+++ function.php
@@ -0,0 +1,14 @@
+<?php
+
+function say_name($var = "bichin")
+{
+    echo $var;
+}
+
+function get_site_title(){ 
+    return "RDKSM";
+}
+
+function get_class_body($page) {
+    return $page;
+}
---
+++ index.php
@@ -0,0 +1,11 @@
+<?php 
+$page = "home";
+include 'function.php'; 
+include 'header.php'; 
+say_name("Savio");
+?>
+
+<?php
+include 'footer.php'; 
+?>
+
